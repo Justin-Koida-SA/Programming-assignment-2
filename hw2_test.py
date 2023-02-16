d35cb43486e84bd32b7f2350631d7f67452b636b
Justin-Koida-SA <100797123+Justin-Koida-SA@users.noreply.github.com>
2023-02-15 23:14:01 -0800

    Update hw2_test.py

--- hw2_test.py
+++ hw2_test.py
@@ -1,7 +1,31 @@
 import data
 import unittest
 import math
-import hw2.py
+import hw2
 
 
-class TestCase:
+class TestCases(unittest.TestCase):
+
+    def test_create_rectangle1(self):
+        point1 = data.Point(7, 12)
+        point2 = data.Point(-4, 8)
+        create = hw2.create_rectangle(point1, point2)
+        self.assertEqual(create, data.Rectangle((-4, 12), (7, 8)))
+
+    def test_create_rectangle2(self):
+        point1 = data.Point(-14, -4)
+        point2 = data.Point(-1, -8)
+        create = hw2.create_rectangle(point1, point2)
+        self.assertEqual(create, data.Rectangle((-14, -4), (-1, -8)))
+
+
+
+
+
+
+
+
+
+
+if __name__ == "main":
+    unittest.main()
