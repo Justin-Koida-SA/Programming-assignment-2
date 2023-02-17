91572fe6ece41d1f72692e77be68adb7063fe8e0
Justin-Koida-SA <100797123+Justin-Koida-SA@users.noreply.github.com>
2023-02-16 17:05:04 -0800

    Update hw2_test.py

--- hw2_test.py
+++ hw2_test.py
@@ -18,6 +18,20 @@ class TestCases(unittest.TestCase):
         create = hw2.create_rectangle(point1, point2)
         self.assertEqual(create, data.Rectangle((-14, -4), (-1, -8)))
 
+    def test_short_than1(self):
+        duration1 = data.Duration(4, 30)
+        duration2 = data.Duration(3, 20)
+        time = hw2.shorter_duration_than(duration1, duration2)
+        self.assertEqual(time, False)
+
+    def test_short_than2(self):
+        duration1 = data.Duration(4, 40)
+        duration2 = data.Duration(5, 30)
+        time = hw2.shorter_duration_than(duration1, duration2)
+        self.assertEqual(time, True)
+
+    def test_song_shorter_than1(self):
+        list = [data.Song('Jimmy', 'School', data.Duration(4, 20))]
 
 
 
