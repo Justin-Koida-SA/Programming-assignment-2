49fa326f03bf30f7e2f8e696e1e734f8c45a18a1
Justin-Koida-SA <100797123+Justin-Koida-SA@users.noreply.github.com>
2023-02-16 17:04:58 -0800

    Update data_tests.py

--- data_tests.py
+++ data_tests.py
@@ -3,46 +3,6 @@ import unittest
 
 
 class TestCases(unittest.TestCase):
-    def test_Price_1(self):
-        price = data.Price(1, 20)
-        self.assertEqual(price.dollars, 1)
-        self.assertEqual(price.cents, 20)
-
-
-    def test_Price_2(self):
-        price = data.Price(4, 19)
-        self.assertEqual(price.dollars, 4)
-        self.assertEqual(price.cents, 19)
-
-
-    def test_Price_eq_1(self):
-        price1 = data.Price(1, 20)
-        price2 = data.Price(1, 20)
-        self.assertEqual(price1, price2)
-
-
-    def test_Price_eq_2(self):
-        price1 = data.Price(1, 20)
-        self.assertEqual(price1, price1)
-
-
-    def test_Price_eq_3(self):
-        price1 = data.Price(1, 20)
-        price2 = data.Price(2, 20)
-        self.assertNotEqual(price1, price2)
-
-
-    def test_Price_eq_4(self):
-        price = data.Price(1, 20)
-        other = 1.20
-        self.assertNotEqual(price, other)
-
-
-    def test_Price_repr(self):
-        price = data.Price(5, 75)
-        self.assertEqual(repr(price), 'Price(5, 75)')
-
-
     def test_Point_1(self):
         point = data.Point(7, 20)
         self.assertAlmostEqual(point.x, 7)
@@ -83,6 +43,94 @@ class TestCases(unittest.TestCase):
         self.assertEqual(repr(point), 'Point(5, 75)')
 
 
+    def test_Duration_1(self):
+        duration = data.Duration(1, 20)
+        self.assertEqual(duration.minutes, 1)
+        self.assertEqual(duration.seconds, 20)
+
+
+    def test_Duration_2(self):
+        duration = data.Duration(4, 19)
+        self.assertEqual(duration.minutes, 4)
+        self.assertEqual(duration.seconds, 19)
+
+
+    def test_Duration_eq_1(self):
+        duration1 = data.Duration(1, 20)
+        duration2 = data.Duration(1, 20)
+        self.assertEqual(duration1, duration2)
+
+
+    def test_Duration_eq_2(self):
+        duration1 = data.Duration(1, 20)
+        self.assertEqual(duration1, duration1)
+
+
+    def test_Duration_eq_3(self):
+        duration1 = data.Duration(1, 20)
+        duration2 = data.Duration(2, 20)
+        self.assertNotEqual(duration1, duration2)
+
+
+    def test_Duration_eq_4(self):
+        duration = data.Duration(1, 20)
+        other = 1.20
+        self.assertNotEqual(duration, other)
+
+
+    def test_Duration_repr(self):
+        duration = data.Duration(5, 14)
+        self.assertEqual(repr(duration), 'Duration(5, 14)')
+
+
+    def test_Song_1(self):
+        song = data.Song('Artist', 'Title', data.Duration(3, 21))
+        self.assertEqual(song.artist, 'Artist')
+        self.assertEqual(song.title, 'Title')
+        self.assertEqual(song.duration, data.Duration(3, 21))
+
+
+    def test_Song_eq_1(self):
+        song1 = data.Song('Artist', 'Title', data.Duration(3, 21))
+        song2 = data.Song('Artist', 'Title', data.Duration(3, 21))
+        self.assertEqual(song1, song2)
+
+
+    def test_Song_eq_2(self):
+        song = data.Song('Artist', 'Title', data.Duration(3, 21))
+        self.assertEqual(song, song)
+
+
+    def test_Song_eq_3(self):
+        song1 = data.Song('Artist1', 'Title', data.Duration(3, 21))
+        song2 = data.Song('Artist2', 'Title', data.Duration(3, 21))
+        self.assertNotEqual(song1, song2)
+
+
+    def test_Song_eq_4(self):
+        song1 = data.Song('Artist', 'Title1', data.Duration(3, 21))
+        song2 = data.Song('Artist', 'Title2', data.Duration(3, 21))
+        self.assertNotEqual(song1, song2)
+
+
+    def test_Song_eq_5(self):
+        song1 = data.Song('Artist', 'Title', data.Duration(2, 21))
+        song2 = data.Song('Artist', 'Title', data.Duration(3, 21))
+        self.assertNotEqual(song1, song2)
+
+
+    def test_Song_eq_6(self):
+        song = data.Song('Artist', 'Title', data.Duration(2, 21))
+        other = 1.20
+        self.assertNotEqual(song, other)
+
+
+    def test_Song_repr(self):
+        song = data.Song('Artist', 'Title', data.Duration(2, 21))
+        self.assertEqual(repr(song),
+            "Song('Artist', 'Title', Duration(2, 21))")
+
+
     def test_Rectangle_1(self):
         rectangle = data.Rectangle(data.Point(7, 20), data.Point(12, 10))
         self.assertEqual(rectangle.top_left, data.Point(7, 20))
@@ -116,128 +164,6 @@ class TestCases(unittest.TestCase):
         rectangle = data.Rectangle(data.Point(7, 20), data.Point(12, 10))
         self.assertEqual(repr(rectangle),
             'Rectangle(Point(7, 20), Point(12, 10))')
-
-
-    def test_Circle_1(self):
-        circle = data.Circle(data.Point(7, 20), 6.2)
-        self.assertEqual(circle.center, data.Point(7, 20))
-        self.assertEqual(circle.radius, 6.2)
-
-
-    def test_Circle_eq_1(self):
-        circle1 = data.Circle(data.Point(7, 20), 6.2)
-        circle2 = data.Circle(data.Point(7, 20), 6.2)
-        self.assertEqual(circle1, circle2)
-
-
-    def test_Circle_eq_2(self):
-        circle = data.Circle(data.Point(7, 19), 4.7)
-        self.assertEqual(circle, circle)
-
-
-    def test_Circle_eq_3(self):
-        circle1 = data.Circle(data.Point(7, 20), 4.7)
-        circle2 = data.Circle(data.Point(4, 17), 4.7)
-        self.assertNotEqual(circle1, circle2)
-
-
-    def test_Circle_eq_4(self):
-        circle1 = data.Circle(data.Point(7, 20), 4.7)
-        circle2 = data.Circle(data.Point(7, 20), 4.6)
-        self.assertNotEqual(circle1, circle2)
-
-
-    def test_Circle_eq_5(self):
-        circle = data.Circle(data.Point(7, 20), 4.7)
-        other = 1.20
-        self.assertNotEqual(circle, other)
-
-
-    def test_Circle_repr(self):
-        circle = data.Circle(data.Point(7, 20), 2)
-        self.assertEqual(repr(circle), 'Circle(Point(7, 20), 2)')
-
-
-    def test_Book_1(self):
-        book = data.Book(['Neil Gaiman', 'Terry Pratchett'], 'Good Omens')
-        self.assertEqual(book.authors, ['Neil Gaiman', 'Terry Pratchett'])
-        self.assertEqual(book.title, 'Good Omens')
-
-
-    def test_Book_eq_1(self):
-        book1 = data.Book(['Neil Gaiman', 'Terry Pratchett'], 'Good Omens')
-        book2 = data.Book(['Neil Gaiman', 'Terry Pratchett'], 'Good Omens')
-        self.assertEqual(book1, book2)
-
-
-    def test_Book_eq_2(self):
-        book = data.Book(['Neil Gaiman', 'Terry Pratchett'], 'Good Omens')
-        self.assertEqual(book, book)
-
-
-    def test_Book_eq_3(self):
-        book1 = data.Book(['Neil Gaiman', 'Terry Pratchett'], 'Good Omens')
-        book2 = data.Book(['Terry Pratchett', 'Neil Gaiman'], 'Good Omens')
-        self.assertNotEqual(book1, book2)
-
-
-    def test_Book_eq_4(self):
-        book1 = data.Book(['Neil Gaiman', 'Terry Pratchett'], 'Good Omens')
-        book2 = data.Book(['Neil Gaiman', 'Terry Pratchett'],
-            'Illustrated Good Omens')
-        self.assertNotEqual(book1, book2)
-
-
-    def test_Book_eq_5(self):
-        book = data.Book(['Neil Gaiman', 'Terry Pratchett'], 'Good Omens')
-        other = 1.20
-        self.assertNotEqual(book, other)
-
-
-    def test_Book_repr(self):
-        book = data.Book(['Neil Gaiman', 'Terry Pratchett'], 'Good Omens')
-        self.assertEqual(repr(book),
-            "Book(['Neil Gaiman', 'Terry Pratchett'], 'Good Omens')")
-
-
-    def test_Employee_1(self):
-        employee = data.Employee('Neil', 5)
-        self.assertEqual(employee.name, 'Neil')
-        self.assertEqual(employee.pay_rate, 5)
-
-
-    def test_Employee_eq_1(self):
-        employee1 = data.Employee('Neil', 5)
-        employee2 = data.Employee('Neil', 5)
-        self.assertEqual(employee1, employee2)
-
-
-    def test_Employee_eq_2(self):
-        employee = data.Employee('Neil', 5)
-        self.assertEqual(employee, employee)
-
-
-    def test_Employee_eq_3(self):
-        employee1 = data.Employee('Neil', 5)
-        employee2 = data.Employee('Terry', 5)
-        self.assertNotEqual(employee1, employee2)
-
-
-    def test_Employee_eq_4(self):
-        employee1 = data.Employee('Neil', 5)
-        employee2 = data.Employee('Neil', 7)
-        self.assertNotEqual(employee1, employee2)
-
-
-    def test_Employee_eq_5(self):
-        employee = data.Employee('Neil', 5)
-        other = 1.20
-        self.assertNotEqual(employee, other)
-
-
-    def test_Employee_repr(self):
-        employee = data.Employee('Neil', 5)
-        self.assertEqual(repr(employee), "Employee('Neil', 5)")
 
 
 if __name__ == '__main__':
